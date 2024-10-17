9349944bc9778522aabd3ba66e3d5dfb7b05d80f
Bill <ashew@wwu.edu>
2024-10-17 01:59:28 -0700

    renaming source folder

---
+++ source/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using LibreMonitor;
+
+namespace BtwMetrics{
+    class MakeTextfileMetrics
+    {
+        static void Main()
+        {
+            LibreMonitor.Monitor _monitor = new LibreMonitor.Monitor();
+            while(true){
+                File.WriteAllText("C:\\Program Files\\windows_exporter\\textfile_inputs\\cpu.prom",_monitor.Cpu());
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
