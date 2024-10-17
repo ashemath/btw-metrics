ae16b2f5f1624e00d946881c2b83f1245d57eaa5
Bill <ashew@wwu.edu>
2024-10-17 02:26:54 -0700

    Adding missing file back to source.

---
+++ source/BtwMetrics.cs
@@ -0,0 +1,16 @@
+namespace BtwMetrics 
+{
+    /* 
+     * BtwTextMetrics
+     * The BtwTextMetrics class is a trivial object that
+     * holds onto an empty string.
+     */
+    public class BtwTextMetrics {
+        string text = "";
+
+        public void MetricString(string Text){
+            text = Text;
+        }
+
+    }
+}
